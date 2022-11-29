719547592a513b890850171f672096f89dbddfea
Arthur Torres <arthur-2008@hotmail.com>
2022-11-29 12:55:56 -0300

    first react

---
+++ src/Components/Contato.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './Contato.module.css'
+import foto from '../img/contato.jpg'
+import Head from './Head'
+
+const Contato = () => {
+  return (
+    <section className={`${styles.contato} animeLeft`}>
+      <Head title="Ranek | Contato"
+        description="Entre em Contato"
+      />
+      <img src={foto} alt="maquina de escrever" />
+      <div>
+        <h1>Entre em contato.</h1>
+        <ul className={styles.dados}>
+          <li>tuzin@trembala.com</li>
+          <li>+55 (22)99782-2626</li>
+          <li>Rua Abius - Cabo Frio</li>
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export default Contato;
